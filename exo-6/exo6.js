ad5728ac33535b99bab416503cfa7c6a4b445794
Romain Wirth <wirth.romain@gmail.com>
2023-05-23 17:05:33 +0200

    exo-6 premiere partie complété

--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -11,7 +11,15 @@ var joeInfo = {
 // ! Ne faites pas de modification au dessus de cette ligne !
 
 // 1. Afficher le nombre de voitures de Joe
+let joesCarsNumbers = joeInfo.cars;
+console.log("Joe a :",joesCarsNumbers.length,"voitures");
 
 // 2. Changer le nombre de salles de bains de Joe : il n'en possède qu'une. Afficher le nouveau nombre de salles de bain.
+joeInfo.bathrooms = 1;
+console.log("Joe a :",joeInfo.bathrooms,"salle de bains");
 
 // 3. Joe vient d'acquérir un garage. Afficher cette nouvelle information. 
+joeInfo.garage = true;
+console.log("Joe possède un garage :",joeInfo.garage);
+
+console.log(joeInfo);
