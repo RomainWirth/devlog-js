1c7051d33a6d8f5955b0df77e6f80b28c39a2690
Romain Wirth <wirth.romain@gmail.com>
2023-05-23 16:53:41 +0200

    finalisation exo-5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,18 @@
 // you can write js here
-
 console.log('exo-5');
+
+let input = "J'aime le chocolat";
+let vowels = ['A', 'E', 'I', 'O', 'U', 'Y'];
+let resultArray = [];
+
+for (i = 0; i < input.length; i++) {
+    //console.log(input[i]);
+    for (j = 0; j < vowels.length; j++) {
+        if (input[i].toUpperCase() == vowels[j]) {
+            resultArray.push(input[i].toUpperCase());
+        }
+    }
+}
+console.log("tableau final :",resultArray);
+let arrayToString = resultArray.join('');
+console.log(arrayToString);
