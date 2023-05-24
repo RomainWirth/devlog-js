03f01a960a534ef65dc3faa7fb27d4db6e4df678
Romain Wirth <wirth.romain@gmail.com>
2023-05-24 11:25:57 +0200

    fin étape 11

--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -12,17 +12,17 @@ var joeInfo = {
 
 // 1. Afficher le nombre de voitures de Joe
 let joesCarsNumbers = joeInfo.cars;
-console.log("Joe a :",joesCarsNumbers.length,"voitures");
+// console.log("Joe a :",joesCarsNumbers.length,"voitures");
 
 // 2. Changer le nombre de salles de bains de Joe : il n'en possède qu'une. Afficher le nouveau nombre de salles de bain.
 joeInfo.bathrooms = 1;
-console.log("Joe a :",joeInfo.bathrooms,"salle de bains");
+// console.log("Joe a :",joeInfo.bathrooms,"salle de bains");
 
 // 3. Joe vient d'acquérir un garage. Afficher cette nouvelle information. 
 joeInfo.garage = true;
-console.log("Joe possède un garage :",joeInfo.garage);
+// console.log("Joe possède un garage :",joeInfo.garage);
 
-console.log(joeInfo);
+// console.log(joeInfo);
 
 // Suite exercice 
 let team = {};
@@ -50,6 +50,23 @@ team = {
             teamPoints,
             opponentPoints
         })
+    },
+    teamPointsCalculus: function() {
+        let sum = 0;
+        for (i = 0; i < this.games.length; i++) {
+            // console.log(this.games[i].teamPoints);
+            sum += this.games[i].teamPoints;
+        }
+        return sum;
+    },
+    opponentPointsAverage: function() {
+        let sum = 0;
+        for (i = 0; i < this.games.length; i++) {
+            // console.log(this.games[i].opponentPoints);
+            sum += this.games[i].opponentPoints;
+        }
+        let average = sum / this.games.length;
+        return average;        
     }
 };
 team.addPlayer("Toto","Pimpin", 9);
@@ -57,22 +74,15 @@ team.addPlayer("Lucas", "Hoang", 22);
 team.addPlayer("Ugo", "Wadoux", 24);
 team.addPlayer("Ezra", "Strikwerda", 35);
 
-team.addGame("OuiOui", 35, 50);
-team.addGame("Naruto", 20, 80);
-team.addGame("Luffy", 60, 40);
-team.addGame("Goku", 75, 60);
+team.addGame("OuiOui", 35, 35);
+team.addGame("Naruto", 20, 75);
+team.addGame("Luffy", 60, 35);
+team.addGame("Goku", 75, 55);
 
-console.log(team);
+// console.log(team);
 
+let totalTeamPoints = team.teamPointsCalculus();
+console.log(totalTeamPoints);
 
-
-        // let newPlayer = {
-        //     firstName: firstName,
-        //     lastName: lastName,
-        //     age: arguments
-        // };
-        // this.players.push(newPlayer);
-
-
-
-        // team.addPlayer("Toto","Pimpin", 9);
+let averageOpponentPoints = team.opponentPointsAverage();
+console.log(averageOpponentPoints);
