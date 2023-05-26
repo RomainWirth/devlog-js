5cedf436eda2db92ac8bd7f2e329f670299ee760
Romain Wirth <wirth.romain@gmail.com>
2023-05-26 16:06:10 +0200

    etape 5 validée

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -14,34 +14,63 @@ let translateType = {
     show: "Spectacles"
 }
 
-for (let article of jsonDatas) {
-    console.log(article);
-    // console.log("type de produit (eng) :", article.type);
-    article.translatedType = translateType[article.type];
-
+const render = (list) => {
     let itemsContainer = document.getElementById("allItems");
-
-    let newItemSheet = document.createElement("div");
-    newItemSheet.className = "itemSheet";
-    console.log(newItemSheet);
-    itemsContainer.appendChild(newItemSheet);
-
-    const createStructure = (titre, valeur, valueSuffix) => {
-        return `
-        <div class="itemType" style="width : 50%; display: flex; justify-content: space-between;">
-            <h4>${titre} :</h4>
-            <p>${valeur} ${valueSuffix ?? ''}</p>
-        </div>
+    itemsContainer.innerHTML = '';
+    for (let article of list) {
+        // console.log(article);
+        // console.log("type de produit (eng) :", article.type);
+        article.translatedType = translateType[article.type];
+        // console.log("type traduit :", article.translatedType.toLowerCase());
+    
+        let newItemSheet = document.createElement("div");
+        newItemSheet.className = "itemSheet";
+        itemsContainer.appendChild(newItemSheet);
+    
+        const createStructure = (titre, valeur, valueSuffix) => {
+            return `
+                <div class="itemType" style="width : 50%; display: flex; justify-content: space-between;">
+                    <h4>${titre} :</h4>
+                    <p>${valeur} ${valueSuffix ?? ''}</p>
+                </div>
+            `
+        }
+        
+        newItemSheet.innerHTML = `
+            <h3 class="itemName">${article.name}</h3>
+            ${createStructure('Catégorie', article.translatedType)}
+            ${createStructure('Description', article.description)}
+            ${createStructure('Prix', article.price, ' €')}
+            ${createStructure('Quantité en stock', article.quantity)}
         `
     }
-
-    newItemSheet.innerHTML = `
-        <h3 class="itemName">${article.name}</h3>
-        ${createStructure('Catégorie', article.translatedType)}
-        ${createStructure('Description', article.description)}
-        ${createStructure('Prix', article.price, ' €')}
-        ${createStructure('Quantité en stock', article.quantity)}
-    `
 }
-console.log("nouveau tableau de produits :", jsonDatas);
+render(jsonDatas);
+console.log(jsonDatas);
+let button = document.querySelector('button');
+button.addEventListener('click', function() {
+    let input = document.getElementById('searchField').value.toLowerCase();
+    console.log("input result :", input);
+
+    const dataMap = new Map(Object.entries(translateType));
+    // console.log(dataMap);
+    const dataArray = Array.from(dataMap.values());
+    // console.log(dataArray);
+    for (i = 0; i < dataArray.length; i++){
+        dataArray[i] = dataArray[i].toLocaleLowerCase();
+    }
+    console.log(dataArray);
+    if (dataArray.includes(input)) {
+        console.log("test ok !");
+        let jsonDatasCategory = jsonDatas.filter(product => product.translatedType.toLowerCase() === input);
+        console.log("tableau filtré : ",jsonDatasCategory);
+        render(jsonDatasCategory);
+    } 
+    // else {
+    //     alert ('merci d\'entrer une catégorie valide : voitures, maison, jeu, veux vidéos, spectacles');
+    //     location.reload();
+    // }
+});
+
+// console.log("nouveau tableau de produits :", jsonDatas);
 
