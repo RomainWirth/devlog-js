1a25fc3fce604626d891f687e80efc528ff3bf25
Romain Wirth <wirth.romain@gmail.com>
2023-06-01 11:21:01 +0200

    derniere modif exo 6 pour validation compétences

--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -1,115 +1,115 @@
 // Cours 8 de Codecademy
-console.log("exo-6");
+console.log("exo-6"); 
 
-var joeInfo = {
-    name: "Joe's house",
-    rooms: 5,
-    garage: false,
-    bathrooms: 1 + 2,
-    cars: ['Clio', 'Van'],
-};
-// ! Ne faites pas de modification au dessus de cette ligne !
+var joeInfo = { 
+    name: "Joe's house", 
+    rooms: 5, 
+    garage: false, 
+    bathrooms: 1 + 2, 
+    cars: ['Clio', 'Van'], 
+}; 
+// ! Ne faites pas de modification au dessus de cette ligne ! 
 
-// 1. Afficher le nombre de voitures de Joe
-let joesCarsNumbers = joeInfo.cars;
-// console.log("Joe a :",joesCarsNumbers.length,"voitures");
+// 1. Afficher le nombre de voitures de Joe 
+let joesCarsNumbers = joeInfo.cars; 
+// console.log("Joe a :",joesCarsNumbers.length,"voitures"); 
 
-// 2. Changer le nombre de salles de bains de Joe : il n'en possède qu'une. Afficher le nouveau nombre de salles de bain.
-joeInfo.bathrooms = 1;
-// console.log("Joe a :",joeInfo.bathrooms,"salle de bains");
+// 2. Changer le nombre de salles de bains de Joe : il n'en possède qu'une. Afficher le nouveau nombre de salles de bain. 
+joeInfo.bathrooms = 1; 
+// console.log("Joe a :",joeInfo.bathrooms,"salle de bains"); 
 
-// 3. Joe vient d'acquérir un garage. Afficher cette nouvelle information. 
-joeInfo.garage = true;
-// console.log("Joe possède un garage :",joeInfo.garage);
+// 3. Joe vient d'acquérir un garage. Afficher cette nouvelle information.  
+joeInfo.garage = true; 
+// console.log("Joe possède un garage :",joeInfo.garage); 
 
-// console.log(joeInfo);
+// console.log(joeInfo); 
 
-// Suite exercice 
-let team = {};
-team = {
-    players: [{
-        firstName: "Pablo",
-        lastName: "Sanchez",
-        age: 11
-    }],
-    games: [{
-        opponent: "Broncos",
-        teamPoints: 42,
-        opponentPoints: 27
-    }],
-    addPlayer(firstName, lastName, age) {
-        this.players.push({
-            firstName, 
+// Suite exercice  
+let team = {}; 
+team = { 
+    players: [{ 
+        firstName: "Pablo", 
+        lastName: "Sanchez", 
+        age: 11 
+    }], 
+    games: [{ 
+        opponent: "Broncos", 
+        teamPoints: 42, 
+        opponentPoints: 27 
+    }], 
+    addPlayer(firstName, lastName, age) { 
+        this.players.push({ 
+            firstName,  
             lastName, 
-            age
-        });
-    },
-    addGame(opponent, teamPoints, opponentPoints) {
-        this.games.push({
-            opponent,
-            teamPoints,
-            opponentPoints
-        })
-    },
-    teamPointsCalculus() {
-        let sum = 0;
-        for (i = 0; i < this.games.length; i++) {
-            // console.log(this.games[i].teamPoints);
-            sum += this.games[i].teamPoints;
-        }
-        return sum;
-    },
-    opponentPointsAverage() {
-        let sum = 0;
-        for (i = 0; i < this.games.length; i++) {
-            // console.log(this.games[i].opponentPoints);
-            sum += this.games[i].opponentPoints;
-        }
-        let average = sum / this.games.length;
-        return average;        
-    },
-    mostAgedPlayer() {
-        let ages = [];
-        for (i = 0; i < this.players.length; i++) {
-            ages.push(this.players[i].age);
-            // console.log(typeof(ages[i]));
-        }
-        ages;
-        // console.log(ages);
+            age 
+        }); 
+    }, 
+    addGame(opponent, teamPoints, opponentPoints) { 
+        this.games.push({ 
+            opponent, 
+            teamPoints, 
+            opponentPoints 
+        }) 
+    }, 
+    teamPointsCalculus() { 
+        let sum = 0; 
+        for (i = 0; i < this.games.length; i++) { 
+            // console.log(this.games[i].teamPoints); 
+            sum += this.games[i].teamPoints; 
+        } 
+        return sum; 
+    }, 
+    opponentPointsAverage() { 
+        let sum = 0; 
+        for (i = 0; i < this.games.length; i++) { 
+            // console.log(this.games[i].opponentPoints); 
+            sum += this.games[i].opponentPoints; 
+        } 
+        let average = sum / this.games.length; 
+        return average; 
+    }, 
+    mostAgedPlayer() { 
+        let ages = []; 
+        for (i = 0; i < this.players.length; i++) { 
+            ages.push(this.players[i].age); 
+            // console.log(typeof(ages[i])); 
+        } 
+        ages; 
+        // console.log(ages); 
 
-        let maximum = Math.max(...ages);
-        // console.log(maximum);
+        let maximum = Math.max(...ages); 
+        // console.log(maximum); 
 
-        let oldestPlayer = this.players.find(function(player) {
-            return player.age === maximum;
-        });
-        // console.log(oldestPlayer);
-        return oldestPlayer;
-    },
-    sortPlayers() {
-        return this.players.sort((player1, player2) => player1.lastName.localeCompare(player2.lastName));
-    }
-};
-team.addPlayer("Toto","Pimpin", 9);
-team.addPlayer("Lucas", "Hoang", 22);
-team.addPlayer("Ugo", "Wadoux", 24);
-team.addPlayer("Ezra", "Strikwerda", 35);
+        let oldestPlayer = this.players.find(function(player) { 
+            return player.age === maximum; 
+        }); 
+        // console.log(oldestPlayer); 
+        return oldestPlayer; 
+    }, 
+    sortPlayers() { 
+        return this.players.sort((player1, player2) => player1.lastName.localeCompare(player2.lastName)); 
+    } 
+}; 
+team.addPlayer("Toto","Pimpin", 9); 
+team.addPlayer("Lucas", "Hoang", 22); 
+team.addPlayer("Ugo", "Wadoux", 24); 
+team.addPlayer("Ezra", "Strikwerda", 35); 
 
-team.addGame("OuiOui", 35, 35);
-team.addGame("Naruto", 20, 75);
-team.addGame("Luffy", 60, 35);
-team.addGame("Goku", 75, 55);
+team.addGame("OuiOui", 35, 35); 
+team.addGame("Naruto", 20, 75); 
+team.addGame("Luffy", 60, 35); 
+team.addGame("Goku", 75, 55); 
 
-console.log(team);
+console.log(team); 
 
-let totalTeamPoints = team.teamPointsCalculus();
-console.log(totalTeamPoints);
+let totalTeamPoints = team.teamPointsCalculus(); 
+console.log(totalTeamPoints); 
 
-let averageOpponentPoints = team.opponentPointsAverage();
-console.log(averageOpponentPoints);
+let averageOpponentPoints = team.opponentPointsAverage(); 
+console.log(averageOpponentPoints); 
 
-let myVar = team.mostAgedPlayer();
-console.log(myVar);
+let myVar = team.mostAgedPlayer(); 
+console.log(myVar); 
 
-let sortedPlayers = team.sortPlayers();
-console.log(sortedPlayers);
+let sortedPlayers = team.sortPlayers(); 
+console.log(sortedPlayers); 
