3303a9fcc78dd7d47e1c50f1c1d79bd250e6cbd6
Romain Wirth <wirth.romain@gmail.com>
2023-06-01 11:07:43 +0200

    derniere modif exo 3 pour validation compétences

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,78 +1,78 @@
 // you can write js here
-console.log('exo-3');
+console.log('exo-3'); 
 
-const ROCK = 'rock';
-const PAPER = 'paper';
-const SCISSORS = 'scissors';
-const CHEAT = 'bomb';
+const ROCK = 'rock'; 
+const PAPER = 'paper'; 
+const SCISSORS = 'scissors'; 
+const CHEAT = 'bomb'; 
 
-let playerInput = prompt("Your choice (rock, paper, scissors) ?");
-//console.log(playerInput);
-//console.log(typeof playerInput);
+let playerInput = prompt("Your choice (rock, paper, scissors) ?"); 
+//console.log(playerInput); 
+//console.log(typeof playerInput); 
 
-const getPlayerChoice = (playerInput) => {
-    playerInput = playerInput.toLowerCase();
-    if (playerInput == ROCK || playerInput == PAPER || playerInput == SCISSORS || playerInput == CHEAT) {
-         return playerInput;
-    } else {
-        alert("Merci d'entrer l'un des mots suivants : rock, paper ou scissors");
-        console.log("Erreur, le champs n'a pas été saisi correcetement !");
-        location.reload();
-    }
-} // end function
+const getPlayerChoice = (playerInput) => { 
+    playerInput = playerInput.toLowerCase(); 
+    if (playerInput == ROCK || playerInput == PAPER || playerInput == SCISSORS || playerInput == CHEAT) { 
+         return playerInput; 
+    } else { 
+        alert("Merci d'entrer l'un des mots suivants : rock, paper ou scissors"); 
+        console.log("Erreur, le champs n'a pas été saisi correcetement !"); 
+        location.reload(); 
+    } 
+} // end function 
 
-const getComputerChoice = () => {
-    let randomNumber = Math.floor(Math.random() * (2 - 0) + 2);
-    // console.log(randomNumber);
-    let result;
-    if (randomNumber == 0) {
-        result = ROCK;
-    }
-    if (randomNumber == 1) {
-        result = PAPER;
-    }
-    if (randomNumber == 2) {
-        result = SCISSORS;
-    }
-    // console.log(result);
-    return result;
-} // end function
+const getComputerChoice = () => { 
+    let randomNumber = Math.floor(Math.random() * (2 - 0) + 2); 
+    // console.log(randomNumber); 
+    let result; 
+    if (randomNumber == 0) { 
+        result = ROCK; 
+    } 
+    if (randomNumber == 1) { 
+        result = PAPER; 
+    } 
+    if (randomNumber == 2) { 
+        result = SCISSORS; 
+    } 
+    // console.log(result); 
+    return result; 
+} // end function 
 
 const findWinner = (playerChoice, computerChoice) => { 
-    if (playerChoice == computerChoice) {
-        return 'Tied';
-    }
-    if (playerChoice === ROCK) {
-        if (computerChoice === SCISSORS) {
-            return 'Won';
-        }
-        return 'Lost';
-    }
-    if (playerChoice === PAPER) {
-        if (computerChoice === ROCK) {
-            return 'Won';
-        } 
-        return 'Lost';
-    }
-    if (playerChoice === SCISSORS) {
-        if (computerChoice === PAPER) {
-            return 'Won';
-        }
-        return 'Lost';
-    }
-    if (playerChoice === CHEAT) {
-        return 'Won';
+    if (playerChoice == computerChoice) { 
+        return 'Tied'; 
     } 
-} // end function
+    if (playerChoice === ROCK) { 
+        if (computerChoice === SCISSORS) { 
+            return 'Won'; 
+        } 
+        return 'Lost'; 
+    } 
+    if (playerChoice === PAPER) { 
+        if (computerChoice === ROCK) { 
+            return 'Won'; 
+        } 
+        return 'Lost'; 
+    } 
+    if (playerChoice === SCISSORS) { 
+        if (computerChoice === PAPER) { 
+            return 'Won'; 
+        } 
+        return 'Lost'; 
+    } 
+    if (playerChoice === CHEAT) { 
+        return 'Won'; 
+    } 
+} // end function 
 
-const playGame = () => {
-    let uChoice = getPlayerChoice(playerInput);
-    console.log(uChoice);
-    let computerChoice = getComputerChoice();
-    console.log(computerChoice);
-    let winner = findWinner(uChoice, computerChoice);
-    console.log(winner);
-    return winner;
-}
+const playGame = () => { 
+    let uChoice = getPlayerChoice(playerInput); 
+    console.log(uChoice); 
+    let computerChoice = getComputerChoice(); 
+    console.log(computerChoice); 
+    let winner = findWinner(uChoice, computerChoice); 
+    console.log(winner); 
+    return winner; 
+} 
 
-playGame();
+playGame(); 
